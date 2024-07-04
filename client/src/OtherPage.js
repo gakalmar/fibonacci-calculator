64e12918ca839600e8defa57aee4f6d59fde0dd5
Gabor Kalmar <gakalmar@gmail.com>
2024-07-04 16:18:38 +0200

    Create OtherPage for navigation

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <Link to="/">Back</Link>
+        </div>
+    )
+}
