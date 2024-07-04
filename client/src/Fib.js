4f2cd562ecd872b2b0c7c0d596d88c644ecefcb0
Gabor Kalmar <gakalmar@gmail.com>
2024-07-04 16:36:28 +0200

    Update Fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -22,4 +22,50 @@ class Fib extends Component {
         const seenIndexes = await axios.get('/api/values/all');
         this.setState({ seenIndexes: seenIndexes.data });
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('/api/values', {
+            index: this.state.index
+        });
+        this.setState({ index: '' });
+    }
+
+    renderSeenIndexes() {
+        return this.state.seenIndexes.map(({ number }) => number).join(', ')
+    }
+
+    renderValues() {
+        const entries = [];
+        for (let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} the result was {this.state.values[key]}
+                </div>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your index:</label>
+                    <input 
+                        value ={this.state.index}
+                        onChange={event => this.setState({ index: event.target.value })}
+                    />
+                    <button>Submit</button>
+                </form>
+                <h3>Known indices:</h3>
+                {this.renderSeenIndexes()}
+                <h3>Calculated values:</h3>
+                {this.renderValues()}
+
+            </div>
+        )
+    }
 }
+
+export default Fib;
