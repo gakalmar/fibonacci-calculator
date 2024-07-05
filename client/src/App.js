50a1efb64f1d385707e23aa2106694bd479945ee
Gabor Kalmar <gakalmar@gmail.com>
2024-07-05 15:29:39 +0200

    Update App and OtherPage components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -7,18 +7,27 @@ import Fib from './Fib';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Fibonacci calculator</h1>
-        <Link to="/">Home</Link>
-        <Link to="/otherpage">Other</Link>
-      </header>
-      <div>
-        <Route exact path="/" component={Fib} />
-        <Route path="/otherpage" component={OtherPage}/>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+const OtherPage = () => {
     return (
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            <Link to="/">Back</Link>
-        </div>
-    )
-}
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <Link to="/">Back</Link>
+      </div>
+    );
+  };
+  
+  export default OtherPage;
